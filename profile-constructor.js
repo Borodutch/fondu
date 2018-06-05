7b731228651bd947f80c93945d9c9764849d673b
backmeupplz <backmeupplz@gmail.com>
2018-06-05 00:07:44 -0700

    Initial commit

---
+++ profile-constructor.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#profile-constructor',
+  data: {
+    title: '',
+    description: '',
+    titleSample: 'ACME ICO',
+    descriptionSample: 'ACME is the most innovative motor industry that came up with the concept of conducting electicity from the thin air! Just turn on the generator and static current from the Earth will start flowing into your appliences.',
+    team: '',
+    teamSample: `John Doe — CEO of ACME and leading expert in the ingeneering community. Has registered various patents including shock-resistant rubber ducks and the wheel inductory mechanism of future. He is the guy who came up with the idea to suck electricity out of nowhere.
+    
+    Sir Low Inn — prodigy developer who worked at Facemovie, Doodle, Amatron, Yoodle! and other tech companies. He is the most recognizable blockchain enthusiast which is why we have to keep him guarded 24/7 — he knows one too many secrets.`,
+    legal: '',
+    legalSample: 'Our tokens are neither securities nor utilities — they are just imaginary money that we can exchange for good. You bring imaginary money — we give you real goods. Deal?'
+  }
+})
