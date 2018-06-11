f4ee1310a4961617053b8770ece5d8d11a23affb
backmeupplz <backmeupplz@gmail.com>
2018-06-11 00:45:16 -0700

    change token rate

--- contract/contracts/FDUCrowdsale.sol
+++ contract/contracts/FDUCrowdsale.sol
@@ -30,6 +30,7 @@ contract FDUCrowdsale is MintedCrowdsale, TimedCrowdsale, Ownable {
     } else if (block.timestamp < 1546300800) {
       rate = rate * 2;
     }
+    rate = rate / 10000000000000000;
     return _weiAmount.mul(actualRate);
   }
 }
--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -4,7 +4,7 @@ const crowdsale = artifacts.require("../contracts/FDUCrowdsale.sol");
 module.exports = function(deployer, network, accounts) {
     const openingTime = 1530403200; // 1 July 2018
     const closingTime = 1561939200; // 1 July 2019
-    const rate = new web3.BigNumber(250); // 250 FDU per 1 ETH (2.5e-16) // TODO: fix
+    const rate = new web3.BigNumber(2.5); // 250 FDU per 1 wei
     const wallet = accounts[1];
     return deployer
         .then(() => {
