a99884011d5015d9d48268176b43d5a516168764
backmeupplz <backmeupplz@gmail.com>
2018-06-27 23:49:14 -0700

    fix english texts

--- profile-constructor.js
+++ profile-constructor.js
@@ -4,12 +4,12 @@ var app = new Vue({
     title: '',
     description: '',
     titleSample: 'ACME ICO',
-    descriptionSample: 'ACME is the most innovative motor industry that came up with the concept of conducting electicity from the thin air! Just turn on the generator and static current from the Earth will start flowing into your appliences.',
+    descriptionSample: 'ACME is the most innovative company of the energy industry, recently implementing our patented technology of conducting electricity from thin air! Just turn on the generator and static current from the Earth will start flowing into your appliances.',
     team: '',
-    teamSample: `John Doe — CEO of ACME and leading expert in the ingeneering community. Has registered various patents including shock-resistant rubber ducks and the wheel inductory mechanism of future. He is the guy who came up with the idea to suck electricity out of nowhere.
-    
-    Sir Low Inn — prodigy developer who worked at Facemovie, Doodle, Amatron, Yoodle! and other tech companies. He is the most recognizable blockchain enthusiast which is why we have to keep him guarded 24/7 — he knows one too many secrets.`,
+    teamSample: `John Doe: CEO of ACME and leading expert in electrical engineering. He has registered various patents including shock-resistant rubber ducks and the wheel inductor mechanism of the future. He is famous for inventing the process for generating electricity out of thin air.
+
+    Sir Low Inn: a prodigy developer with experience from Facemovie, Doodle, Amatron, Yoodle! and other tech companies, he is the most recognizable blockchain developer in the world, which is why we have chosen to keep him under lock and key 24/7, as he knows far too many company secrets.`,
     legal: '',
-    legalSample: 'Our tokens are neither securities nor utilities — they are just imaginary money that we can exchange for good. You bring imaginary money — we give you real goods. Deal?'
+    legalSample: 'Our tokens are neither securities nor utilities — they are an imaginary currency that can be exchanged for goods. You bring the imaginary money and we give you real goods. Deal?'
   }
 })
