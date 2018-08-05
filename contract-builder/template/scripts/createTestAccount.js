79af3080c32168516347c366ea138cddcd104d40
backmeupplz <backmeupplz@gmail.com>
2018-08-05 11:31:04 -0700

    update script

--- contract-builder/template/scripts/createTestAccount.js
+++ contract-builder/template/scripts/createTestAccount.js
@@ -5,10 +5,10 @@ module.exports = function(callback) {
   console.log(`Created ETH wallet to deploy contracts: ${account} (password is '123456789', don't loose it)`)
   console.log(`Requesting 1 ETH to ${account} (http://faucet.ropsten.be:3001/donate/${account})`)
   request('GET', `http://faucet.ropsten.be:3001/donate/${account}`)
-  console.log('Requested, waiting on the Ethereum node to sync and then 5 seconds to receive test ETH...')
+  console.log('Requested, waiting on the Ethereum node to sync and then 15 seconds to receive test ETH...')
   setTimeout(() => {
     checkBalance(account, callback)
-  }, 5 * 1000)
+  }, 15 * 1000)
 }
 
 function checkBalance(account, callback) {
@@ -17,9 +17,9 @@ function checkBalance(account, callback) {
     console.log(`Received 1 test ETH successfully (balance is ${web3.fromWei(balance, 'ether')} ETH)`)
     callback()
   } else {
-    console.log('Still waiting on 1 test ETH. Retrying to check balance in 5 seconds...')
+    console.log('Still waiting on 1 test ETH. Retrying to check balance in 15 seconds...')
     setTimeout(() => {
       checkBalance(account, callback)
-    }, 5 * 1000)
+    }, 15 * 1000)
   }
 }
