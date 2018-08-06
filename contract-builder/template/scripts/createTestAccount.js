322362dfc143b9677a1844c366f76f10fb9a0076
backmeupplz <backmeupplz@gmail.com>
2018-08-05 21:17:31 -0700

    add extra error message

--- contract-builder/template/scripts/createTestAccount.js
+++ contract-builder/template/scripts/createTestAccount.js
@@ -4,7 +4,10 @@ module.exports = function(callback) {
   const account = web3.personal.newAccount('123456789')
   console.log(`Created ETH wallet to deploy contracts: ${account} (password is '123456789', don't loose it)`)
   console.log(`Requesting 1 ETH to ${account} (http://faucet.ropsten.be:3001/donate/${account})`)
-  request('GET', `http://faucet.ropsten.be:3001/donate/${account}`)
+  const res = request('GET', `http://faucet.ropsten.be:3001/donate/${account}`)
+  if (res.statusCode >= 300) {
+    throw new Error('Sorry, Ethereum test faucet request failed: ' + res.body.toString())
+  }
   console.log('Requested, waiting on the Ethereum node to sync and then 15 seconds to receive test ETH...')
   setTimeout(() => {
     checkBalance(account, callback)
