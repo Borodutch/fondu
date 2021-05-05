88243eb61c2834350dbeaa438276c4aa39c6b00c
Stas N <stas-nepomn@ya.ru>
2021-05-06 00:32:00 +0800

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: {
-          background: '#040404',
-          'background-purpose': '#12141D',
-          'perk-gradient-light': 'rgba(4, 6, 14, 0.9)',
-          'perk-gradient-dark': '#04060E',
-        },
-        gold: {
-          dark: '#FA6641',
-          light: '#EAE100',
-        },
+        'base-blue': '#6486FF',
+        'base-black': '#05050F',
       },
       borderRadius: {
-        avatar: '6rem',
+        base: '8px',
       },
-      width: {
-        chart: '108px',
-        time: '95px',
-        glass: '107px',
-      },
-    },
-    container: {
-      center: true,
-      padding: '2rem',
     },
   },
-}
+};
