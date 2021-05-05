ddcf8d2c94eb35f93f8395034503dce491ff92ca
Stas N <stas-nepomn@ya.ru>
2021-05-06 01:06:22 +0800

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import React, { useEffect } from 'react'
-import Root from 'components/Root'
-import useSWR from 'swr'
-import { fetcher } from 'helpers/fetcher.helper'
+import React, { useEffect } from "react";
+import Root from "components/Root";
+import useSWR from "swr";
+import { fetcher } from "helpers/fetcher.helper";
 
 const App = () => {
-  const {data, error} = useSWR('https://jsonplaceholder.typicode.com/todos/1', fetcher)
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/todos/1",
+    fetcher
+  );
 
   useEffect(() => {
-    if(data) console.log(data)
-  }, [data])
+    if (data) console.log(data);
+  }, [data]);
 
   return (
     <Root>
       <p>{data && data.title}</p>
     </Root>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react'
-import { classnames } from 'classnames/tailwind'
+import React, { FC } from "react";
+import { classnames } from "classnames/tailwind";
 
-const root = classnames('container', 'mx-auto', 'px-5')
+const root = classnames("container", "mx-auto", "px-5");
 const Root: FC = ({ children }) => {
-  return <div className={root}>{children}</div>
-}
+  return <div className={root}>{children}</div>;
+};
 
-export default Root
+export default Root;
