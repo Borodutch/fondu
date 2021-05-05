90a4db130b2ce8e13db12733c99129e71a53e84c
Stas N <stas-nepomn@ya.ru>
2021-05-06 00:46:54 +0800

    Fetcher setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Root from 'components/Root'
 import useApp from 'hooks/useApp'
 import { BodyText } from 'components/Text'
+import useSWR from 'swr'
+import { fetcher } from 'helpers/fetcher.helper'
 
 const App = () => {
   const { userCount } = useApp()
+  const {data, error} = useSWR('https://jsonplaceholder.typicode.com/todos/1', fetcher)
+
+  useEffect(() => {
+    if(data) console.log(data)
+  }, [data])
 
   return (
     <Root>
-      <BodyText>User count: {userCount}</BodyText>
+      <BodyText>User count: {userCount} and {data && data.title}</BodyText>
     </Root>
   )
 }
