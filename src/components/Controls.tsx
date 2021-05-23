31e67e77223de2b69de3182739c485708db1c465
Arkadiy Kovtun <frozo13@gmail.com>
2021-05-23 04:39:44 +0300

    stepper component

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react"
 import { classnames } from "classnames/tailwind"
+import { hideMobile } from "helpers/style.helper"
 
 const stylizedTextColor = classnames("text-base-blue")
 const stylizedBorderColor = classnames("border-blue-400")
 const stylizedBackgroundColor = classnames("bg-base-blue")
+const stylizedPassedTextColor = classnames("text-blue-200")
+const stylizedPassedBorderColor = classnames("border-blue-200")
+const stylizedPassedBackgroundColor = classnames("bg-blue-200")
 const inactiveTextColor = classnames("text-gray-300")
 const inactiveBorderColor = classnames("border-gray-300")
 const inactiveBackgroundColor = classnames("bg-gray-300")
@@ -73,12 +77,12 @@ const contractButtonStyle = classnames(
   "rounded-base",
   "focus:outline-none"
 )
-export const contractButtonStyleActive = classnames(
+const contractButtonStyleActive = classnames(
   contractButtonStyle,
   stylizedBorderColor,
   stylizedTextColor
 )
-export const contractButtonStyleInactive = classnames(
+const contractButtonStyleInactive = classnames(
   contractButtonStyle,
   "text-gray-600",
   "border-dashed"
@@ -98,5 +102,95 @@ export const ContractButton: FC<
     >
       {children}
     </button>
+  )
+}
+
+const stepperWrapper = classnames(
+  "w-auto",
+  "md:w-4/6",
+  "mx-auto",
+  "flex",
+  "justify-between",
+  "items-center",
+  "pt-3",
+  "pb-8",
+  "space-x-3"
+)
+const singleStepStyle = classnames(
+  "flex",
+  "flex-row",
+  "items-center",
+  "space-x-2"
+)
+const baseTabStyle = classnames(
+  "w-8",
+  "h-8",
+  "text-white",
+  "rounded-full",
+  "transition",
+  "flex",
+  "items-center",
+  "justify-center"
+)
+const activeTabStyle = classnames(baseTabStyle, stylizedBackgroundColor)
+const inactiveTabStyle = classnames(baseTabStyle, inactiveBackgroundColor)
+const passedTabStyle = classnames(baseTabStyle, stylizedPassedBackgroundColor)
+
+const baseTextStyle = classnames(hideMobile, "transition")
+const activeTextStyle = classnames(baseTextStyle, stylizedTextColor)
+const inactiveTextStyle = classnames(baseTextStyle, inactiveTextColor)
+const passedTextStyle = classnames(baseTextStyle, stylizedPassedTextColor)
+
+const baseLineStyle = classnames("border-b-2", "w-10", "md:w-40")
+const inactiveLineStyle = classnames(baseLineStyle, "border-dashed")
+const passedLineStyle = classnames(baseLineStyle, stylizedPassedBorderColor)
+
+interface stepperTabHeaderProps {
+  tabs: Array<String>
+  currentTab: Number
+}
+
+export const StepperTabs: FC<stepperTabHeaderProps> = ({
+  tabs,
+  currentTab,
+}) => {
+  return (
+    <div className={stepperWrapper}>
+      {tabs.map((tab: String, index) => (
+        <>
+          <div key={index} className={singleStepStyle}>
+            <div
+              className={
+                index + 1 === currentTab
+                  ? activeTabStyle
+                  : index < currentTab
+                  ? passedTabStyle
+                  : inactiveTabStyle
+              }
+            >
+              {index + 1}
+            </div>
+            <span
+              className={
+                index + 1 === currentTab
+                  ? activeTextStyle
+                  : index < currentTab
+                  ? passedTextStyle
+                  : inactiveTextStyle
+              }
+            >
+              {tab}
+            </span>
+          </div>
+          {index < tabs.length - 1 && (
+            <div
+              className={
+                index + 1 < currentTab ? passedLineStyle : inactiveLineStyle
+              }
+            ></div>
+          )}
+        </>
+      ))}
+    </div>
   )
 }
--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,128 +1,14 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { AppNetworks, appStore } from "store/app.store"
-import {
-  activeTextStyleReal,
-  activeTextStyleTest,
-  inactiveLineStyle,
-  inactiveTextStyle,
-  passedTextStyleTest,
-  passedTextStyleReal,
-  passedLineStyleReal,
-  passedLineStyleTest,
-  activeTabStyleReal,
-  activeTabStyleTest,
-  passedTabStyleReal,
-  passedTabStyleTest,
-  inactiveTabStyleTest,
-  inactiveTabStyleReal,
-  stepperWrapper,
-  singleStepStyle,
-} from "./styles"
+import { appStore } from "store/app.store"
+import { StepperTabs } from "components/Controls"
 
 const Stepper: FC = () => {
-  const currentActiveTextStyle =
-    appStore.currentNetwork === AppNetworks.Real
-      ? activeTextStyleReal
-      : activeTextStyleTest
-  const currentPassedTextStyle =
-    appStore.currentNetwork === AppNetworks.Real
-      ? passedTextStyleReal
-      : passedTextStyleTest
-  const currentPassedLineStyle =
-    appStore.currentNetwork === AppNetworks.Real
-      ? passedLineStyleReal
-      : passedLineStyleTest
-  const currentActiveTabStyle =
-    appStore.currentNetwork === AppNetworks.Real
-      ? activeTabStyleReal
-      : activeTabStyleTest
-  const currentInactiveTabStyle =
-    appStore.currentNetwork === AppNetworks.Real
-      ? inactiveTabStyleReal
-      : inactiveTabStyleTest
-  const currentPassedTabStyle =
-    appStore.currentNetwork === AppNetworks.Real
-      ? passedTabStyleReal
-      : passedTabStyleTest
-
   return (
-    <div className={stepperWrapper}>
-      <div className={singleStepStyle}>
-        <div
-          className={
-            appStore.currentTab === 1
-              ? currentActiveTabStyle
-              : currentPassedTabStyle
-          }
-        >
-          1
-        </div>
-        <span
-          className={
-            appStore.currentTab === 1
-              ? currentActiveTextStyle
-              : currentPassedTextStyle
-          }
-        >
-          Token type
-        </span>
-      </div>
-      <div
-        className={
-          appStore.currentTab > 1 ? currentPassedLineStyle : inactiveLineStyle
-        }
-      ></div>
-      <div className={singleStepStyle}>
-        <div
-          className={
-            appStore.currentTab === 2
-              ? currentActiveTabStyle
-              : appStore.currentTab > 2
-              ? currentPassedTabStyle
-              : currentInactiveTabStyle
-          }
-        >
-          2
-        </div>
-        <span
-          className={
-            appStore.currentTab === 2
-              ? currentActiveTextStyle
-              : appStore.currentTab > 2
-              ? currentPassedTextStyle
-              : inactiveTextStyle
-          }
-        >
-          Token settings
-        </span>
-      </div>
-      <div
-        className={
-          appStore.currentTab > 2 ? currentPassedLineStyle : inactiveLineStyle
-        }
-      ></div>
-      <div className={singleStepStyle}>
-        <div
-          className={
-            appStore.currentTab === 3
-              ? currentActiveTabStyle
-              : currentInactiveTabStyle
-          }
-        >
-          3
-        </div>
-        <span
-          className={
-            appStore.currentTab === 3
-              ? currentActiveTextStyle
-              : inactiveTextStyle
-          }
-        >
-          Deployment
-        </span>
-      </div>
-    </div>
+    <StepperTabs
+      tabs={["Token type", "Token settings", "Deployment"]}
+      currentTab={appStore.currentTab}
+    />
   )
 }
 
