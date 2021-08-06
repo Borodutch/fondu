41e195ab79ff1a176bea2b40fb0dfb8a10cf745f
RobuASD <Roollexx@live.com>
2021-08-06 22:22:00 +1000

    Add, Replacing language buttons with dropdown list

--- src/components/LanguageButtons.tsx
+++ src/components/LanguageButtons.tsx
@@ -1,4 +1,97 @@
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 import { Language, flagForLanguage } from 'models/Language'
+import { appStore } from 'store/AppStore'
+import { classnames } from 'classnames/tailwind'
+import { FormattedMessage } from 'react-intl'
+
+const buttonStyle = classnames(
+  'border-blue-400',
+  'text-base-blue',
+  'hover:text-white',
+  'hover:bg-base-blue'
+)
+const menuButtonStyle = classnames(
+  'border',
+  'border-blue-400',
+  'inline-flex',
+  'justify-center',
+  'w-full',
+  'px-4',
+  'py-2',
+  'text-base-blue',
+  'rounded-md',
+  'bg-opacity-20',
+  'hover:text-white',
+  'hover:bg-base-blue',
+  'focus:outline-none'
+)
+const menuItemStyle = classnames(
+  'absolute',
+  'right-0',
+  'origin-top-right',
+  'bg-white',
+  'rounded-md',
+  'shadow-lg',
+  'ring-1',
+  'ring-black',
+  'ring-opacity-5',
+  'focus:outline-none'
+)
+
+const selectedButtonStyle = classnames('bg-base-blue', 'text-white')
+
+export default function Example() {
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className={menuButtonStyle}>
+            <FormattedMessage id="language" />
+            <ChevronDownIcon
+              className="w-5 h-5 ml-2 -mr-1"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className={menuItemStyle}>
+            <div className="px-1 py-1 ">
+              {(['en', 'ru'] as Language[]).map((k) => (
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      key={k}
+                      onClick={() => {
+                        appStore.language = k
+                      }}
+                      className={`${
+                        active ? selectedButtonStyle : buttonStyle
+                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                    >
+                      {flagForLanguage(k)}
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  )
+}
+
+/*import { Language, flagForLanguage } from 'models/Language'
 import { appStore } from 'store/AppStore'
 import { classnames } from 'classnames/tailwind'
 import Button from 'components/Button'
@@ -24,4 +117,4 @@ export default function LanguageButtons() {
       ))}
     </div>
   )
-}
+}*/
